4d8b2eec38d22a398044c7d59750fe074075e690
hongloull <hongloull@7243796d-a6ea-a77f-0f6c-a7acc59dc59a>
2010-08-11 14:36:26 +0000

    a

--- trunk/container_ass/dereferenceMapIterator.cc
+++ trunk/container_ass/dereferenceMapIterator.cc
@@ -7,10 +7,23 @@ using namespace std;
 
 // Add elements to container by push_front or push_back or insert
 int dereferenceMapIterator() {
-	vector<string> vector_str(5,"test") ;
+	map<int,string> words ;
+	// insert key and value
+	words[1] = "a" ;
+	// Insert key and value
+	words.insert( map<int,string>::value_type( 2, "b") ) ;
+	// Using make_pair to insert
+	words.insert( make_pair( 3, "c") ) ;
+	// Using typedef to insert
+	typedef map<int,string>::value_type map_value ;
+	words.insert( map_value( 4, "d") ) ;
+
 	map<int,string>::iterator map_it = words.begin() ;
-	cout << map_it->first << endl ;
-	cout << map_it->second << endl ;
+	while( map_it != words.end() ){
+		cout << map_it->first << " " ;
+		cout << map_it->second << endl ;
+		++map_it ;
+	}
 
 	return 0 ;
 }
