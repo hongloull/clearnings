71c62055b1c3e09fdc2369b8e545ed0f7ea143aa
hongloull <hongloull@7243796d-a6ea-a77f-0f6c-a7acc59dc59a>
2010-08-22 16:16:17 +0000

    add statements

---
+++ trunk/statements/preprocessor_debugging.cpp
@@ -0,0 +1,17 @@
+// Using the Preprocessor for Debugging
+#include <iostream>
+using namespace std ;
+
+// If comment below line, then will execute cerr
+//#define NDEBUG
+
+//int main() {
+//#ifndef NDEBUG
+//	cerr << "Error: " <<  __FILE__
+//			<< " : line " << __LINE__<< endl
+//			<< "       Compiled on " << __DATE__
+//			<< " at " << __TIME__ << endl ;
+//#endif
+//
+//	return 0 ;
+//}
